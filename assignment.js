ca3ca14b546f721ab03277e14180a38d6f35264c
Wuraola Olaniyan <122721324+OG-wura@users.noreply.github.com>
2023-07-19 20:02:42 +0100

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,127 @@
+// comments can make code readable
+// welcome to 30 DAYS OF JAVASCRIPT
+/* comments can make code readable
+easy to reuse and informative */
+
+
+// let score = 69.1;
+// if (score <= 100 && score >= 70){
+//   console.log("A1")
+// }
+// else if (score <= 70 && score >= 65)
+// {
+//   console.log("B2")
+// }
+// else console.log("invalid input")
+
+
+let state = "Abia"
+if (state == "Abia" || state == "South Eastern"){
+console.log("The capital of Abia is Umuahia.")
+}
+else if (state == "Adamawa" || state == "North Eastern"){
+  console.log("The capital of Adamawa is Yola.")
+}
+else if (state == "Akwa Ibom"  && state == "Southern" ){
+  console.log("The capital of Akwa Ibom is Uyo.")
+}
+else if (state == "Anambra" || state == "South Eastern"){
+  console.log("The capital of Anambra is Akwa.")
+}
+else if(state == "Bauchi" && state == "North Eastern"){
+  console.log("The capital of Bauchi is Bauchi.")
+}
+else if(!state == "London" || state == "Bayelsa"){
+  console.log("The capital of Bayelsa is Yenagoa and it's located at the south south part of nigeria.")
+}
+else if(state == "Benue" || state == "Food Basket"){
+  console.log("The capital of Benue is Makurdi.");
+}
+else if(state == "Borno" || state == "Terrorist"){
+  console.log("The capital of Borno is Maiduguri.");
+}
+else if(state == "Cross River" && state == "People's paradise"){
+  console.log("The capital of Cross River is Calabar.")
+}
+else if(state == "Delta" || state == "East"){
+  console.log("The capital of Delta is Asaba.")
+}
+else if(state == "Ebonyi" && state == "East"){
+  console.log("The capital of Ebonyi is Abakaliki.")
+}
+else if(state == "Edo" || state == "Heart Beat"){
+  console.log("The capital of Edo is Benin City.");
+}
+else if(state == "Ekiti" && state == "South Western"){
+  console.log("The capital of Ekiti is Ado Ekiti.")
+}
+else if(state == "Enugu" || state == "Coal City"){
+  console.log("The capital of Enugu is Enugu.")
+}
+else if(state == "Gombe" || state == "North Eastern"){
+  console.log("The capital of Gombe is Gombe.")
+}
+else if(state == "Imo" && state == "Largest City"){
+  console.log("The capital of Imo is Owerri.")
+}
+else if(state == "Jigawa" || state == "North Western"){
+  console.log("The capital of Jigawa is Dutse.")
+}
+else if(state == "Kaduna" && state == "North Western"){
+  console.log("The capital of Kaduna is Kaduna.")
+}
+else if(state == "Kano" || state == "Northern"){
+  console.log("The capital of Kano is Kano.")
+}
+else if(state == "Katsina" || state == "Western"){
+  console.log("The capital of Katsina is Katsina.")
+}
+else if(state == "Kebbi" && state == "Land Of Equity"){
+  console.log("The capital of Kebbi is Birnin Kebbi.");
+}
+else if(state == "Kogi" && state == "North Central"){
+  console.log("The capital of Kogi is Lokoja.")
+}
+else if(state == "Kwara" || state == "West Central"){
+  console.log("The capital of Kwara is Ilorin.")
+}
+else if(state == "Lagos" && state == "South Western"){
+  console.log("The capital of Lagos is Ikeja.")
+}
+else if(state == "Nasarawa" || state == "Central Nigeria"){
+  console.log("The capital of Nasarawa is Lafia.")
+}
+else if(state == "Niger" && state == "North Central"){
+  console.log("The capital of Niger is Minna.")
+}
+else if(state == "Ogun" && state == "South Western"){
+  console.log("The capital of Ogun is Abeokuta.")
+}
+else if(state == "Ondo" || state == "South Western"){
+  console.log("The capital of Ondo is Akure.")
+}
+else if(state == "Osun" && state == "South Eastern"){
+  console.log("The capital of Osun is Oshogbo.")
+}
+else if(state == "Oyo" || state == "South Western"){
+  console.log("The capital of Oyo is Ibadan.")
+}
+else if(state == "Plateau" || state == "East Central"){
+  console.log("The capital of Plateau is Jos.")
+}
+else if(state == "Rivers" || state == "Treasure Base"){
+  console.log("The capital of Rivers is Port Harcourt.")
+}
+else if(state == "Sokoto" && state == "Northern"){
+  console.log("The capital of Sokoto is Sokoto.")
+}
+else if(state == "Taraba" || state == "Middle Belt"){
+  console.log("The capital of Taraba is Jalingo.")
+}
+else if(state == "Yobe" && state == " North Eastern"){
+  console.log("The capital of Yobe is Damaturu.");
+}
+else if(state == "Zamfara" || state == "North West"){
+  console.log("The capital of Zamfara is Gusau.")
+}
+else console.log("This state is not in nigeria!")
